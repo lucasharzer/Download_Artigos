c0ce8ba1a23391663651ebbc4240070a6970041e
Lucas de Matos <lucasmatos592@gmail.com>
2024-05-27 23:14:36 -0300

    Scrapping Bs4 New

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import requests
 import platform
 import aiohttp
 import asyncio
@@ -9,7 +10,7 @@ import os
 class Navigation:
     def __init__(self):
         # Main variables
-        self.link = "https://journal.seriousgamessociety.org/index.php/IJSG/issue/archive"
+        self.link = "https://www.hindawi.com/journals/ijcgt/contents"
         self.folder = os.path.join(os.getcwd(), "files")
         self.titles_file = "titles.txt"
         self.pos_lock = asyncio.Lock()
@@ -28,78 +29,57 @@ class Navigation:
         while True:
             try:
                 # Page content
-                async with aiohttp.ClientSession() as session:
-                    async with session.get(url) as response:
-                        response.raise_for_status()
-                        # HTML content
-                        soup = BeautifulSoup(await response.text(), 'html.parser')
-                        return soup
-            except aiohttp.ClientResponseError as e:
-                return 0
+                headers = {
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+                }
+                response = requests.get(url, headers=headers)
+                # # HTML content
+                soup = BeautifulSoup(response.text, 'html.parser')
+                return soup 
+            except requests.exceptions.RequestException as e:
+                return 0             
             except Exception as e:
-                print(f"Ocorreu um erro: {e}")
+                print(f"Ocorreu um erro: {type(e)}")
+                # Wait 3 seconds to repet the request after some error
+                await asyncio.sleep(3)
 
-    async def download_file(self, url, file_name):
-        soup = await self.access_url(url)
-        if soup != 0:
-            # PDF link
-            download_link = soup.find('a', class_='download')['href']
-            if not download_link.startswith('http'):
-                download_link = aiohttp.helpers.urljoin(url, download_link)
+    async def download_file(self, download_link, file_name):
+        # PDF Download
+        async with aiohttp.ClientSession() as session:
+            async with session.get(download_link) as pdf_response:
+                pdf_response.raise_for_status()
+                pdf_content = await pdf_response.read()
 
-            # PDF Download
-            async with aiohttp.ClientSession() as session:
-                async with session.get(download_link) as pdf_response:
-                    pdf_response.raise_for_status()
-                    pdf_content = await pdf_response.read()
-
-            # Save PDF
-            file_path = os.path.join(self.folder, file_name)
-            with open(file_path, 'wb') as file:
-                file.write(pdf_content)
-            return True
-        else:
-            return False
-
-    async def access_volumes(self, soup, pos):
+        # Save PDF
+        file_path = os.path.join(self.folder, file_name)
+        with open(file_path, 'wb') as file:
+            file.write(pdf_content)
+        
+    async def access_articles(self, soup, pos):
         item = total_items = 0
-
+        
         with open(self.titles_file, "a", encoding="utf-8") as txt_file:
-            volumes = soup.find_all("a", class_="title")
-            print(f"Accessing {len(volumes)} volumes, wait some seconds...")
-            # Get total items
-            for volume in volumes:
-                soup_volumes = await self.access_url(volume["href"])
-                articles = soup_volumes.find_all("a", class_="obj_galley_link pdf")
-                total_items += len(articles)
+            contents = soup.find_all("div", class_="sc-dxZgTM iYdecD toc_article")
+            total_items = len(contents)
+            tasks = []
+            for content in contents:
+                title = content.find("h2").get_text().split("\n")[0]
+                # PDF link
+                download_url = content.find("a", attrs={"aria-label": "Download PDF"})["href"]
+                async with self.pos_lock:
+                    pos += 1
+                    file_name = f"{pos}-" + re.sub(r'[<>:"/\\|?*]', "", title) + ".pdf"
+                
+                item += 1
+                print(f"[{item}/{total_items}] - \033[94m{title[:30]+'...'}\033[0m", end="\r")
 
-            # Access each volume
-            for volume in volumes:
-                soup_volumes = await self.access_url(volume["href"])
-                articles = soup_volumes.find_all("a", class_="obj_galley_link pdf")
-
-                # Access each link
-                tasks = []
-                for article in articles:
-                    item += 1
-                    title = str(soup_volumes.find(
-                        "a", id=re.search(r"article-\d+", article["id"]).group()
-                    ).get_text()).strip().split("\n")[0]
-
-                    async with self.pos_lock:
-                        pos += 1
-                        file_name = f"{pos}-" + re.sub(r'[<>:"/\\|?*]', "", title) + ".pdf"
-
-                    print(f"[{item}/{total_items}] - \033[94m{title[:30]+'...'}\033[0m", end="\r")
-
-                    tasks.append((self.download_file(article['href'], file_name), title, pos))
-
-                results = await asyncio.gather(*[task[0] for task in tasks])
-                for result, task in zip(results, tasks):
-                    if result:
-                        _, title, current_pos = task
-                        txt_file.write(f"{current_pos}.{title}\n")
-                        txt_file.flush()
+                tasks.append((self.download_file(download_url, file_name), title, pos))
+            
+            results = await asyncio.gather(*[task[0] for task in tasks])
+            for result, task in zip(results, tasks):
+                _, title, current_pos = task
+                txt_file.write(f"{current_pos}.{title}\n")
+                txt_file.flush()
 
         return pos
 
@@ -109,12 +89,16 @@ class Navigation:
                 # Access page
                 print(f"Accessing the link: \033[92m{self.link}\033[0m")
                 soup = await self.access_url()
-                pos = await self.access_volumes(soup, pos)
+                pos = await self.access_articles(soup, pos)
                 # Next page
-                self.link = soup.find("a", class_="next")["href"]
-            except TypeError:
-                print("End of pages" + " "*35)
-                break
+                self.link = str(soup.find("a", attrs={"aria-label": "Next"})["href"])
+                # Wait 3 seconds to avoid "Too Many Requests" error
+                await asyncio.sleep(3)
+                if self.link == "#/":
+                    print("End of pages" + " "*35)
+                    break
+            except Exception as e:
+                print(f"Ocorreu um erro: {e}")    
 
     def list_files(self):
         files = len(os.listdir(self.folder))
